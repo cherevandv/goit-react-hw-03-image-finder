998f346448f25c25041c3ec504722596088ace9c
Daria Kobekhno <cherevandv@gmail.com>
2021-01-04 17:37:42 +0200

    fixed bugs

--- src/services/news-api.js
+++ src/services/news-api.js
@@ -1,6 +1,9 @@
+const baseUrl = 'https://pixabay.com/api/';
+const APIkey = '19547883-c83eecf3fafc0cf8c7a25c2e3';
+
 export function fetchImages(q, currentPage) {
   return fetch(
-    `https://pixabay.com/api/?q=${q}&page=${currentPage}&key=19547883-c83eecf3fafc0cf8c7a25c2e3&image_type=photo&orientation=horizontal&per_page=12`,
+    `${baseUrl}?q=${q}&page=${currentPage}&key=${APIkey}&image_type=photo&orientation=horizontal&per_page=12`,
   )
     .then(res => res.json())
     .then(data =>
@@ -12,5 +15,3 @@ export function fetchImages(q, currentPage) {
       })),
     );
 }
-
-// export default { fetchImages };
